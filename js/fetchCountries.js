a7a36b1ab372935d71b92b3949ef19109877c594
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-08 21:15:43 +0300

    1

--- js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-export function fetchCountries(countryName) {
-  const filter = '?fields=name,capital,population,flags,languages';
-  return fetch(
-    `https://restcountries.com/v3.1/name/${countryName}${filter}`
-  ).then(response => {
-    return response.json();
-  });
-}
