b1af64d701d810c45a43da27934ad87d055d11f4
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-09 22:49:49 +0300

    5

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import food from '../js/data.json';
 const salatButton = document.querySelector('[data-salat]');
 const omletButton = document.querySelector('[data-omlet]');
 const drinkButton = document.querySelector('[data-drink]');
+const foodsEl = document.getElementsByClassName('hero__paragraph-list');
 
 const arrayEl = document.querySelector('[data-array]');
 const mainEl = document.querySelector('[data-main]');
@@ -37,7 +38,7 @@ function openListArray() {
   arrayEl.classList.toggle('hero__array-open');
   mainEl.classList.toggle('hero__main-open');
   closeEl.classList.toggle('hidden');
-  paragraphEl.classList.toggle('hidden');
+  paragraphEl.classList.add('hidden');
   arrayEl.classList.add('animateForvard');
   arrayEl.classList.remove('animateBack');
   mainEl.classList.add('animateForvardMain');
@@ -49,18 +50,38 @@ function onClose() {
   arrayEl.classList.remove('animateForvard');
   mainEl.classList.remove('animateForvardMain');
   mainEl.classList.add('animateBacMain');
-  arrayEl.removeChild(document.querySelector('.hero__array-list'));
+
+  function callback() {
+    arrayEl.removeChild(document.querySelector('.hero__array-list'));
+    paragraphEl.classList.remove('hidden');
+  }
+
+  setTimeout(callback, 700);
 }
 
 function makeSalat() {
-  const commonFinal = `
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>`;
+  const counter = 6;
+  const salatArray = [];
+  const arrayFinal = [];
+
+  for (let i = 0; i < counter; i += 1) {
+    let maxNumber = food[0].common.length - 1;
+    let randomNumber = (Math.random() * (maxNumber - 0) + 0).toFixed(0);
+    if (!salatArray.includes(randomNumber)) {
+      salatArray.push(randomNumber);
+    }
+  }
+
+  salatArray.map(number => {
+    let mark = `<li class="inner-item">${food[0].common[number]}</li>`;
+    console.log(number, food[0].common[number]);
+    arrayFinal.push(mark);
+  });
+
+  const commonFinal = arrayFinal.join('');
+
   const salatFinal = `<li class="inner-item">${food[0].salat}</li>`;
+
   const markup = `
   <ul class="hero__array-list">
   <li class="hero__array-item"><h2 class="hero__array-title">Нарізати і перемішати:</h2>
@@ -70,7 +91,7 @@ function makeSalat() {
     <ul class="inner-list">${salatFinal}</ul>
   </li>
   </ul>`;
-  console.log(markup);
+  // console.log(markup);
   arrayEl.insertAdjacentHTML('afterbegin', markup);
 }
 
@@ -80,13 +101,27 @@ function makeOmlet() {
   <li class="inner-item">${food[0].omlet[1]}</li>
   <li class="inner-item">${food[0].omlet[2]}</li>`;
 
-  const commonFinal = `
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[1]}</li>
-  <li class="inner-item">${food[0].common[2]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[1]}</li>
-  <li class="inner-item">${food[0].common[2]}</li>`;
+  const counter = 6;
+  const omletArray = [];
+  const arrayFinal = [];
+
+  for (let i = 0; i < counter; i += 1) {
+    let maxNumber = food[0].common.length - 1;
+    let randomNumber = (Math.random() * (maxNumber - 0) + 0).toFixed(0);
+    console.log(randomNumber);
+    if (!omletArray.includes(randomNumber)) {
+      if (randomNumber !== 13) {
+        omletArray.push(randomNumber);
+      }
+    }
+  }
+
+  omletArray.map(number => {
+    let mark = `<li class="inner-item">${food[0].common[number]}</li>`;
+    arrayFinal.push(mark);
+  });
+
+  const commonFinal = arrayFinal.join('');
 
   const randomSause = (Math.random() * (4 - 0) + 0).toFixed(0);
   const sause = `<li class="inner-item">${food[0].sause[randomSause]}</li>`;
@@ -106,27 +141,64 @@ function makeOmlet() {
 }
 
 function makeDrink() {
-  const omletFinal = `
-  <li class="inner-item">Молоко</li>`;
+  const drinkFinal = `
+  <li class="inner-item">Молоко</li>
+  <li class="inner-item">Морозиво</li>`;
 
-  const commonFinal = `
-  <li class="inner-item">${food[0].common[0]}</li>
-  <li class="inner-item">${food[0].common[1]}</li>
-  <li class="inner-item">${food[0].common[2]}</li>
-  <li class="inner-item">${food[0].common[0]}</li>`;
+  const counter = 4;
+  const fruitsArray = [];
+  const arrayFinal = [];
 
-  const randomSause = (Math.random() * (4 - 0) + 0).toFixed(0);
-  const sause = `<li class="inner-item">${food[0].sause[randomSause]}</li>`;
+  for (let i = 0; i < counter; i += 1) {
+    let maxNumber = food[0].fruits.length - 1;
+    let randomNumber = (Math.random() * (maxNumber - 0) + 0).toFixed(0);
+    console.log(randomNumber);
+    if (!fruitsArray.includes(randomNumber)) {
+      fruitsArray.push(randomNumber);
+    }
+  }
+
+  fruitsArray.map(number => {
+    let mark = `<li class="inner-item">${food[0].fruits[number]}</li>`;
+    arrayFinal.push(mark);
+  });
+
+  const fruitsFinal = arrayFinal.join('');
+
   const markup = `
   <ul class="hero__array-list">
   <li class="hero__array-item"><h2 class="hero__array-title">Залити в блендер:</h2>
-    <ul class="inner-list">${omletFinal}</ul>
+    <ul class="inner-list">${drinkFinal}</ul>
   </li>
   <li class="hero__array-item"><h2 class="hero__array-title">Додати в блендер до молока:</h2>
-    <ul class="inner-list">${commonFinal}</ul>
+    <ul class="inner-list">${fruitsFinal}</ul>
   </li>
   <ul>`;
   arrayEl.insertAdjacentHTML('afterbegin', markup);
 }
 
-console.log(food[0].omlet);
+console.log(foodsEl.length);
+
+const listOfClass = ['big', 'yellow', 'background'];
+
+function garlandCallback() {
+  let randomElement = (Math.random() * (foodsEl.length - 1) + 1).toFixed(0);
+  let randomClass = (Math.random() * (listOfClass.length - 1) + 1).toFixed(0);
+  foodsEl[randomElement].classList.add(`${listOfClass[randomClass - 1]}`);
+  setTimeout(smallCallback, 250);
+
+  function smallCallback() {
+    foodsEl[randomElement].classList.remove(`${listOfClass[randomClass - 1]}`);
+  }
+  const clone = foodsEl[0].cloneNode(true);
+  paragraphEl.appendChild(clone);
+  foodsEl[0].remove();
+}
+
+function intervalCallback() {
+  setInterval(garlandCallback, 500);
+}
+
+setInterval(garlandCallback, 500);
+
+setTimeout(intervalCallback, 250);
