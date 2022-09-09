5b86df8b04f08acedc143f2daeec45a83a29cf26
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-09 18:55:00 +0300

    4

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../sass/main.css';
+import food from '../js/data.json';
 
 const salatButton = document.querySelector('[data-salat]');
 const omletButton = document.querySelector('[data-omlet]');
@@ -12,16 +13,19 @@ const paragraphEl = document.querySelector('[data-paragraph]');
 
 const closeEl = document.querySelector('[data-close]');
 
-salatButton.addEventListener('click', () => {
-  openListArray();
-});
-
 omletButton.addEventListener('click', () => {
   openListArray();
+  makeSalat();
+});
+
+salatButton.addEventListener('click', () => {
+  openListArray();
+  makeOmlet();
 });
 
 drinkButton.addEventListener('click', () => {
   openListArray();
+  makeDrink();
 });
 
 closeEl.addEventListener('click', () => {
@@ -45,4 +49,84 @@ function onClose() {
   arrayEl.classList.remove('animateForvard');
   mainEl.classList.remove('animateForvardMain');
   mainEl.classList.add('animateBacMain');
+  arrayEl.removeChild(document.querySelector('.hero__array-list'));
 }
+
+function makeSalat() {
+  const commonFinal = `
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>`;
+  const salatFinal = `<li class="inner-item">${food[0].salat}</li>`;
+  const markup = `
+  <ul class="hero__array-list">
+  <li class="hero__array-item"><h2 class="hero__array-title">Нарізати і перемішати:</h2>
+    <ul class="inner-list">${commonFinal}</ul>
+  </li>
+  <li class="hero__array-item"><h2 class="hero__array-title">Полити зверху:</h2>
+    <ul class="inner-list">${salatFinal}</ul>
+  </li>
+  </ul>`;
+  console.log(markup);
+  arrayEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+function makeOmlet() {
+  const omletFinal = `
+  <li class="inner-item">${food[0].omlet[0]}</li>
+  <li class="inner-item">${food[0].omlet[1]}</li>
+  <li class="inner-item">${food[0].omlet[2]}</li>`;
+
+  const commonFinal = `
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[1]}</li>
+  <li class="inner-item">${food[0].common[2]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[1]}</li>
+  <li class="inner-item">${food[0].common[2]}</li>`;
+
+  const randomSause = (Math.random() * (4 - 0) + 0).toFixed(0);
+  const sause = `<li class="inner-item">${food[0].sause[randomSause]}</li>`;
+  const markup = `
+  <ul class="hero__array-list">
+  <li class="hero__array-item"><h2 class="hero__array-title">Змішати та почати смажити:</h2>
+    <ul class="inner-list">${omletFinal}</ul>
+  </li>
+  <li class="hero__array-item"><h2 class="hero__array-title">Нарізати і додати на сковороду через кілька хвилин:</h2>
+    <ul class="inner-list">${commonFinal}</ul>
+  </li>
+  <li class="hero__array-item"><h2 class="hero__array-title">Вкінці додати соус:</h2>
+    <ul class="inner-list">${sause}</ul>
+  </li>
+  <ul>`;
+  arrayEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+function makeDrink() {
+  const omletFinal = `
+  <li class="inner-item">Молоко</li>`;
+
+  const commonFinal = `
+  <li class="inner-item">${food[0].common[0]}</li>
+  <li class="inner-item">${food[0].common[1]}</li>
+  <li class="inner-item">${food[0].common[2]}</li>
+  <li class="inner-item">${food[0].common[0]}</li>`;
+
+  const randomSause = (Math.random() * (4 - 0) + 0).toFixed(0);
+  const sause = `<li class="inner-item">${food[0].sause[randomSause]}</li>`;
+  const markup = `
+  <ul class="hero__array-list">
+  <li class="hero__array-item"><h2 class="hero__array-title">Залити в блендер:</h2>
+    <ul class="inner-list">${omletFinal}</ul>
+  </li>
+  <li class="hero__array-item"><h2 class="hero__array-title">Додати в блендер до молока:</h2>
+    <ul class="inner-list">${commonFinal}</ul>
+  </li>
+  <ul>`;
+  arrayEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+console.log(food[0].omlet);
